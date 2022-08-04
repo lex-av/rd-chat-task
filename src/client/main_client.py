d98e78603c3b46d8f2f70ec9c531fc850c79f406
alex <allexander0509@gmail.com>
2022-08-04 14:44:52 +0300

    Asyncio based websockets was chosen as main tool in project, updated requirements. Serverside: implemented functions to add and remove users from server username/websocket mapping, register users and choose state mode of users. Clientside: implemented functions to register on server and choose user state on server.

--- src/client/main_client.py
+++ src/client/main_client.py
@@ -3,6 +3,50 @@ import asyncio
 import websockets
 
 
+async def register(server_ws):
+    """
+    Tries to register on server by given username
+    :param server_ws: Selected server's websocket
+    :return: None
+    """
+
+    while True:
+        user_name = input("Enter your username ")
+        await server_ws.send(user_name)
+        user_name_answer = await server_ws.recv()
+
+        if bool(int(user_name_answer)):
+            print("Username registered")
+            break
+        else:
+            print("Username is already in use")
+
+
+async def choose_mode(server_ws):
+    """
+    User chooses between connection to another user or to
+    stand by a connection from another user
+
+    :param server_ws: Selected server's websocket
+    :return: None
+    """
+
+    while True:
+        mode = input(
+            "Enter your desired state (0 - await for connection from another user or\n 1 for "
+            "connection to another user) "
+        )
+
+        mode_int = int(mode)  # try needed here
+
+        if mode_int != 0 and mode_int != 1:
+            print("Wrong option selected. Try again")
+            continue
+        else:
+            await server_ws.send(mode)
+            break
+
+
 async def connect():
     """
     Main client's function to register on server and
@@ -12,10 +56,8 @@ async def connect():
     uri = "ws://localhost:2024"
 
     async with websockets.connect(uri) as websocket:
-        u_name = input("What's your u_name? ")
-        await websocket.send(u_name)
-        u_name_answ = await websocket.recv()
-        print(f">>> {u_name_answ}")
+        await register(websocket)
+        await choose_mode(websocket)
 
 
 if __name__ == "__main__":
--- src/server/main_server.py
+++ src/server/main_server.py
@@ -3,9 +3,10 @@ import asyncio
 import websockets
 
 USERS_MAPPING = {}  # Maps username to it's websocket
+AWAITING_USERS = set()
 
 
-async def add_user(user_websocket, user_name: str) -> bool:
+async def add_user(user_websocket, user_name: str, users_mapping: dict) -> bool:
     """
     Adds new user to user mapper. Returns True if user was
     added and False otherwise. Prevents adding username, that
@@ -13,14 +14,67 @@ async def add_user(user_websocket, user_name: str) -> bool:
 
     :param user_websocket: Connected user's websocket
     :param user_name: Username, given by user
+    :param users_mapping: Server mapping from username to it's websocket
     :return: bool
     """
 
-    if user_name in USERS_MAPPING:
+    if user_name in users_mapping:
         return False
     else:
-        USERS_MAPPING[user_name] = user_websocket
+        users_mapping[user_name] = user_websocket
         return True
+
+
+async def remove_user(user_name: str, users_mapping: dict):
+    """
+
+    :param user_name:
+    :param users_mapping:
+    :return:
+    """
+
+    users_mapping.pop(user_name)
+
+
+async def register_user(username: str, user_ws, users_mapping: dict):
+    """
+    Allows user to register on server by entering hit own username. If
+    username is already in use func informs user about it and offers user
+    to try again
+    :param username: Username in str, given by connected user
+    :param user_ws: Connected user's websocket
+    :param users_mapping: Server mapping from username to it's websocket
+    :return: None
+    """
+
+    add_user_ok = await add_user(user_ws, username, users_mapping)
+
+    if add_user_ok:
+        await user_ws.send("1")
+        print("users: ", users_mapping)  # DEBUG, DELETE LATER
+    else:
+        await user_ws.send("0")
+
+
+async def user_choose_mode(user_ws, username: str, awaiting_users: set):
+    """
+    User chooses between connection to another user or to
+    stand by a connection from another user
+
+    :param user_ws: Connected user's websocket
+    :param username: Username in str, given by connected user
+    :param awaiting_users: Server set of users in connection awaiting mode
+     corresponding websockets
+    :return: None
+    """
+
+    current_mode = await user_ws.recv()
+
+    if bool(int(current_mode)):
+        print("Not implemented yet")
+    else:
+        awaiting_users.add(username)
+        print(awaiting_users)
 
 
 async def user_connection_handler(websocket):
@@ -31,13 +85,10 @@ async def user_connection_handler(websocket):
     :param websocket: connected user websocket
     """
 
-    user_name = await websocket.recv()
-    add_user_ok = await add_user(websocket, user_name)
-    print("users: ", USERS_MAPPING)
-    if add_user_ok:
-        await websocket.send("Got your u_name")
-    else:
-        await websocket.send("Username already exists")
+    current_username = await websocket.recv()  # Get username from user
+    await register_user(current_username, websocket, USERS_MAPPING)
+    await user_choose_mode(websocket, current_username, AWAITING_USERS)
+    await remove_user(current_username, USERS_MAPPING)
 
 
 async def main():
