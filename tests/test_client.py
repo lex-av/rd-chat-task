1806add8b3f537d66467bc7a19f6b19dc1a90385
alex <allexander0509@gmail.com>
2022-08-07 15:49:41 +0300

    Added tests for client utils, deleted pytest-freezegun from requirements

---
+++ tests/test_client.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.client import generate_verification_msg, verify_username
+
+verify_username_test_data = [
+    ("normal", 0),
+    ("no", 1),
+    ("normalooooooooooooooo", 2),
+    ("1normal", 3),
+    ("nor~", 4),
+]
+
+generate_verification_msg_test_data = [
+    (0, "Username OK"),
+    (1, "Username too short"),
+    (2, "Username too long"),
+    (3, "Username has to start with letter"),
+    (4, "Username contains restricted characters"),
+]
+
+
+@pytest.mark.parametrize("input, expected", verify_username_test_data)
+def test_verify_username_uname(input, expected):
+    assert verify_username(input) == expected
+
+
+@pytest.mark.parametrize("input, expected", generate_verification_msg_test_data)
+def test_generate_verification_msg(input, expected):
+    assert generate_verification_msg(input) == expected
